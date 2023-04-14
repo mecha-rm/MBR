2243ae97edc2edd944d15f1e9d7538e905ef16b2
mecha-rm <roderick.montague243@gmail.com>
2023-04-14 18:59:24 -0400

    Added Cinemachine and Some Scripts
    Added the cinemachine, and some scripts for planning out game features.

---
+++ Assets/Scripts/InitGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is used to initialize the game in the init scene.
+public class InitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: transfer to next scene on first update.
+    }
+}
