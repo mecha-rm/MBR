ae806cbf47fda7e5ebc3aba26cf8ca6993b43ae4
mecha-rm <roderick.montague243@gmail.com>
2023-04-17 22:01:40 -0400

    Working on Implementing Audio Settings System
    Going to copy over the audio settings system from another project and work on implementing it for this one.

--- Assets/Scripts/SystemSettings.cs
+++ Assets/Scripts/SystemSettings.cs
@@ -7,11 +7,36 @@ namespace mbs
     // The settings for the game (e.g., audio).
     public class SystemSettings : MonoBehaviour
     {
+        // // The tag for BGM.
+        // public const string BGM_TAG = "BGM";
+        // 
+        // // The bgm volume.
+        // private float bgmVolume = 1.0F;
+        // 
+        // // The sfx tag.
+        // public const string SFX_TAG = "SFX";
+        // 
+        // // The sfx volume.
+        // private float sfxVolume = 1.0F;
+
         // Start is called before the first frame update
         void Start()
         {
 
         }
+
+        // // Adjusts the BGM volume.
+        // public void AdjustBackgroundMusicVolume(float newVolume)
+        // {
+        //     bgmVolume = Mathf.Clamp01(newVolume);
+        // }
+        // 
+        // // Adjusts the SFX volume.
+        // public void AdjustSoundEffectVolume(float newVolume)
+        // {
+        //     sfxVolume = Mathf.Clamp01(newVolume);
+        // 
+        // }
 
         // Update is called once per frame
         void Update()
