786552f7059d1f90de4ad12f1b56cf7db513b693
mecha-rm <roderick.montague243@gmail.com>
2023-06-11 18:02:57 -0400

    Working on Setting Up Stage + Movement Test
    I'm working on setting up the first stage. I'm also working on testing out object movement with angular rotations enabled.

---
+++ Assets/Scripts/ControlledMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mbs
+{
+    // The movement being controlled.
+    public class ControlledMovement : MonoBehaviour
+    {
+        // The rigidbody.
+        public new Rigidbody rigidbody;
+
+        // The force mode for the rigidbody.
+        public ForceMode forceMode = ForceMode.Impulse;
+
+        // The speed of the entity.
+        public float speed = 30.0F;
+
+        // The max speed of the entity.
+        public float maxSpeed = 50.0F;
+
+        // Set to 'true' to update inputs.
+        public bool updateInput = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Grab the rigidbody if it's not set.
+            if(rigidbody == null)
+                rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Updates the entity inputs.
+        private void UpdateInputs()
+        {
+            // Calculates the movement.
+            Vector3 move = Vector3.zero;
+            move.x = Input.GetAxisRaw("Horizontal");
+            move.z = Input.GetAxisRaw("Vertical");
+
+            // Move the entity.
+            if (move != Vector3.zero)
+            {
+                // Add force to the rigidbody.
+                rigidbody.AddForce(move * Time.deltaTime, forceMode);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Updates the inputs of the user.
+            if (updateInput)
+                UpdateInputs();
+
+            // Clamping the speed.
+            rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+        }
+    }
+}
